346c3c10a99189d0e081a627fe642f7df13e5a4b
ephratica <ephratica@foxmail.com>
2023-06-02 12:59:02 +0800

    a batter way to develop

--- src/router/index.js
+++ src/router/index.js
@@ -12,10 +12,17 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/StudentHome',
+      name: 'student home',
+      component: () => import('../views/StudentHomeView.vue')
+    },
+    {
+      path: '/TeacherHome',
+      name: 'teacher home',
+      component: () => import('../views/TeacherHomeView.vue')
     }
   ]
 })
--- src/views/StudentHomeView.vue
+++ src/views/StudentHomeView.vue
@@ -3,9 +3,45 @@
 </script>
 
 <template>
-
+  <h1>
+    hello
+  </h1>
 </template>
 
 <style scoped>
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
 
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
 </style>
