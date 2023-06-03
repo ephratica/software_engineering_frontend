f466548c5a9c669b5946bd39595e27752386fbf0
ephratica <ephratica@foxmail.com>
2023-06-03 12:38:49 +0800

    fixed bugs

--- src/main.js
+++ src/main.js
@@ -9,10 +9,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import "./assets/css/nucleo-icons.css";
+import "./assets/css/nucleo-svg.css";
+
+import materialKit from "./material-kit";
 
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
+app.use(materialKit);
 
 app.mount('#app')
