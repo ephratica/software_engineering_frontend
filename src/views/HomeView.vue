0c0914fccce39e958b83f04885e3ed032db66723
ephratica <ephratica@foxmail.com>
2023-06-04 23:16:17 +0800

    remove buttons for show

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,8 +6,8 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <TheWelcome />
-    <button type="button" onclick="window.location.href='/StudentHome?id=1'" class="btn btn-success m-1">Student</button>
-    <button type="button" onclick="window.location.href='/TeacherHome?id=4'" class="btn btn-success m-1">Teacher</button>
+<!--    <button type="button" onclick="window.location.href='/StudentHome?id=1'" class="btn btn-success m-1">Student</button>-->
+<!--    <button type="button" onclick="window.location.href='/TeacherHome?id=4'" class="btn btn-success m-1">Teacher</button>-->
 
   </main>
 </template>
