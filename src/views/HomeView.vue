b6b565a2223e41b5e83d3b949ae18681b5426db4
lyz2012009 <pardon233@foxmail.com>
2023-07-01 15:57:26 +0800

    修复一些bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,13 +6,12 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <TheWelcome />
-<<<<<<< HEAD
     <button type="button" onclick="window.location.href='/StudentHome'" class="btn btn-success m-1">Student</button>
     <button type="button" onclick="window.location.href='/TeacherHome'" class="btn btn-success m-1">Teacher</button>
-=======
+
 <!--    <button type="button" onclick="window.location.href='/StudentHome?id=1'" class="btn btn-success m-1">Student</button>-->
 <!--    <button type="button" onclick="window.location.href='/TeacherHome?id=4'" class="btn btn-success m-1">Teacher</button>-->
->>>>>>> 14b258dbb26c13b7a6e96b7ba6e640a0ffc58136
+
 
   </main>
 </template>
