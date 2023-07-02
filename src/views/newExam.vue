b0b2f50d535e7789796f44cf672e5f97a645d4c6
lyz2012009 <pardon233@foxmail.com>
2023-07-02 13:30:52 +0800

    完善出卷系统和cookie

--- src/views/newExam.vue
+++ src/views/newExam.vue
@@ -1,159 +1,177 @@
 <template>
-    <div class="bg-image">
-      <div class="container mt-5">
-        <div class="card shadow bg-blur">
-          <div class="card-header">
-            <h1 class="card-title">发布试题</h1>
+  <div class="container mt-5">
+    <div class="card shadow">
+      <div class="card-header">
+        <h1 class="card-title">发布试题</h1>
+      </div>
+      <div class="card-body">
+        <form @submit.prevent="submitForm">
+          <div class="mb-3">
+            <label for="number" class="form-label">题目编号：</label>
+            <input v-model="number" id="number" type="number" class="form-control" required>
           </div>
-          <div class="card-body">
-            <form @submit.prevent="submitForm">
-              <h2>第一道选择题</h2>
-              <div class="mb-3">
-                <label for="question1" class="form-label">题目：</label>
-                <input v-model="question1" id="question1" type="text" class="form-control form-control-lg" required>
-              </div>
-              <div class="mb-3">
-                <label for="optionA1" class="form-label">选项A：</label>
-                <input v-model="optionA1" id="optionA1" type="text" class="form-control" required>
-              </div>
-              <div class="mb-3">
-                <label for="optionB1" class="form-label">选项B：</label>
-                <input v-model="optionB1" id="optionB1" type="text" class="form-control" required>
-              </div>
-              <div class="mb-3">
-                <label for="optionC1" class="form-label">选项C：</label>
-                <input v-model="optionC1" id="optionC1" type="text" class="form-control" required>
-              </div>
-              <div class="mb-3">
-                <label for="optionD1" class="form-label">选项D：</label>
-                <input v-model="optionD1" id="optionD1" type="text" class="form-control" required>
-              </div>
-              <div class="mb-3">
-                <label for="answer1" class="form-label">答案：</label>
-                <select v-model="answer1" id="answer1" class="form-select" required>
-                  <option value="">请选择答案</option>
-                  <option value="A">A</option>
-                  <option value="B">B</option>
-                  <option value="C">C</option>
-                  <option value="D">D</option>
-                </select>
-              </div>
-  
-              <h2>第二道选择题</h2>
-              <div class="mb-3">
-                <label for="question2" class="form-label">题目：</label>
-                <input v-model="question2" id="question2" type="text" class="form-control form-control-lg" required>
-              </div>
-              <div class="mb-3">
-                <label for="optionA2" class="form-label">选项A：</label>
-                <input v-model="optionA2" id="optionA2" type="text" class="form-control" required>
-              </div>
-              <div class="mb-3">
-                <label for="optionB2" class="form-label">选项B：</label>
-                <input v-model="optionB2" id="optionB2" type="text" class="form-control" required>
-              </div>
-              <div class="mb-3">
-                <label for="optionC2" class="form-label">选项C：</label>
-                <input v-model="optionC2" id="optionC2" type="text" class="form-control" required>
-              </div>
-              <div class="mb-3">
-                <label for="optionD2" class="form-label">选项D：</label>
-                <input v-model="optionD2" id="optionD2" type="text" class="form-control" required>
-              </div>
-              <div class="mb-3">
-                <label for="answer2" class="form-label">答案：</label>
-                <select v-model="answer2" id="answer2" class="form-select" required>
-                  <option value="">请选择答案</option>
-                  <option value="A">A</option>
-                  <option value="B">B</option>
-                  <option value="C">C</option>
-                  <option value="D">D</option>
-                </select>
-              </div>
-  
-              <h2>大题</h2>
-              <div class="mb-3">
-                <label for="question3" class="form-label">题目：</label>
-                <input v-model="question3" id="question3" type="text" class="form-control form-control-lg" required>
-              </div>
-              <div class="mb-3">
-                <label for="answer3" class="form-label">答案：</label>
-                <textarea v-model="answer3" id="answer3" class="form-control" rows="5" required></textarea>
-              </div>
-  
-              <div class="d-grid gap-2">
-                <button type="submit" class="btn btn-primary btn-lg">发布试题</button>
-              </div>
-            </form>
+          <div class="mb-3">
+            <label for="type" class="form-label">题目类型：</label>
+            <select v-model="type" id="type" class="form-select" required>
+              <option value="">请选择题目类型</option>
+              <option value="选择题">选择题</option>
+              <option value="大题">大题</option>
+            </select>
           </div>
-        </div>
+          <div v-if="type === '选择题'">
+            <div class="mb-3">
+              <label for="question" class="form-label">题目：</label>
+              <input v-model="question" id="question" type="text" class="form-control" required>
+            </div>
+            <div class="mb-3">
+              <label for="optionA" class="form-label">选项A：</label>
+              <input v-model="this.optionA" id="optionA" type="text" class="form-control" required>
+            </div>
+            <div class="mb-3">
+              <label for="optionB" class="form-label">选项B：</label>
+              <input v-model="this.optionB" id="optionB" type="text" class="form-control" required>
+            </div>
+            <div class="mb-3">
+              <label for="optionC" class="form-label">选项C：</label>
+              <input v-model="this.optionC" id="optionC" type="text" class="form-control" required>
+            </div>
+            <div class="mb-3">
+              <label for="optionD" class="form-label">选项D：</label>
+              <input v-model="this.optionD" id="optionD" type="text" class="form-control" required>
+            </div>
+            <div class="mb-3">
+              <label for="description" class="form-label">题目描述：</label>
+              <textarea v-model="description" id="description" class="form-control" required></textarea>
+            </div>
+            <div class="mb-3">
+              <label for="answer" class="form-label">答案：</label>
+              <select v-model="answer" id="answer" class="form-select" required>
+                <option value="">请选择答案</option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
+              </select>
+            </div>
+          </div>
+          <div v-if="type === '大题'">
+            <div class="mb-3">
+              <label for="question" class="form-label">题目：</label>
+              <input v-model="question" id="question" type="text" class="form-control" required>
+            </div>
+            <div class="mb-3">
+              <label for="description" class="form-label">题目描述：</label>
+              <textarea v-model="description" id="description" class="form-control" required></textarea>
+            </div>
+            <div class="mb-3">
+              <label for="answer" class="form-label">答案：</label>
+              <textarea v-model="answer" id="answer" class="form-control" required></textarea>
+            </div>
+          </div>
+          <div class="mb-3">
+            <label for="score" class="form-label">分值：</label>
+            <input v-model="score" id="score" type="number" class="form-control" required>
+          </div>
+          <div class="mb-3">
+            <label for="exam" class="form-label">考试：</label>
+            <select v-model="exam" id="exam" class="form-select" required>
+              <option value="">请选择考试</option>
+              <option v-for="exam in exams" :key="exam.id" :value="exam.id">{{ exam.name }}</option>
+            </select>
+          </div>
+          <div class="text-center">
+            <button type="submit" class="btn btn-primary">发布</button>
+          </div>
+        </form>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "PublishQuestion",
-    data() {
-      return {
-        question1: "",
-        optionA1: "",
-        optionB1: "",
-        optionC1: "",
-        optionD1: "",
-        answer1: "",
-        question2: "",
-        optionA2: "",
-        optionB2: "",
-        optionC2: "",
-        optionD2: "",
-        answer2: "",
-        question3: "",
-        answer3: "",
-      };
-    },
-    methods: {
-      submitForm() {
-        // TODO: 发布试题逻辑
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .bg-image {
-    /* background-image: url('../assets/img/OIP.jpg'); */
-    background-size: cover;
-    background-position: center;
-    /* background-attachment: fixed; 添加固定背景位置 */
-    height: 1000vh;
-  }
-  
-  .container {
-    max-width: 800px;
-  }
-  
-  .card {
-    border: none;
-  }
-  
-  .card-header {
-    background-color: rgba(255, 255, 255, 0.8);
-    backdrop-filter: blur(10px);
-  }
-  
-  .bg-blur {
-    background-color: rgba(255, 255, 255, 0.5);
-    backdrop-filter: blur(10px);
-  }
-  
-  .form-label {
-    font-weight: bold;
-  }
-  
-  @media (max-width: 576px) {
-    .card-title {
-      font-size: 2rem;
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { ElNotification } from "element-plus";
+export default {
+  data() {
+    return {
+      number:0,
+      type: '',
+      question: '',
+      optionA: '',
+      optionB: '',
+      optionC: '',
+      optionD: '',
+      answer: '',
+      description: '',
+      score: 0,
+      exam: '',
+      exams: [],
+      isChoice: false,
+      desc:'',
+    }
+  },
+  created() {
+    axios.get('/api/exam/all')
+      .then(response => {
+        this.exams = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  },
+  methods: {
+
+    submitForm() {
+      console.log(this.type)
+      if(this.type==='选择题'){
+        this.isChoice = true
+        this.desc = this.question+':\nA:'+this.optionA+'\nB: '+this.optionB+'\nC: '+this.optionC+'\nD: '+this.optionD
+      }
+      else{
+        this.desc = this.question
+      }
+      const data = {
+        id:null,
+        examId: this.exam,
+        number: this.number,
+        isChoice:this.isChoice,
+        desc:this.desc,
+        stdAnswer:this.answer,
+        maxScore:this.score
+      }
+      console.log(this.exam,data)
+      
+      axios.put('/api/question/add', data)
+      .then(response => {
+        if(response.data==='success'){
+          ElNotification({
+            title: '提交成功',
+            message: "试卷已刷新",
+            type: "success",
+            duration: 3000
+          })
+        }
+        else{
+          ElNotification({
+            title: '提交失败',
+            message: "请重新尝试",
+            type: "error",
+            duration: 3000
+          })
+        }
+      })
+      .catch(error => {
+        console.log(error)
+        // 可以在这里添加错误提示等
+      })
     }
   }
-  </style>
+}
+</script>
+
+<style>
+.card {
+  max-width: 500px;
+  margin: 0 auto;
+}
+</style>
